82afbe946456672895513db379dffad2b02253a1
Vishesh Gupta <vishesh@stanford.edu>
2014-02-07 06:53:09 +0000

    changed routes to match views

--- routes/pasteats.js
+++ routes/pasteats.js
@@ -1,8 +1,8 @@
 exports.view = function(req, res) {
-  res.render('login');
+  res.render('pasteats');
 }
 
 exports.viewById = function(req, res) {
   var id = req.params.id;
-  res.render('login');
+  res.render('pasteats-entry');
 }
