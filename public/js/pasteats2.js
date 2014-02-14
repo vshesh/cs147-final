743679114b9060a7427c70b88b221c1d6e4205d6
ashngu <ashngu@stanford.edu>
2014-02-13 18:59:51 -0800

    delete pop up in pasteats working

--- public/js/pasteats2.js
+++ public/js/pasteats2.js
@@ -1,10 +1,75 @@
 $(document).ready(function() {
 	initializePage();
+
+      $("#btnNo").click(function (e)
+      {
+         HideDialog();
+         e.preventDefault();
+      });
+
+      $("#btnYes").click(function (e)
+      {
+         HideDialog();
+         deleteEntry(e);
+         e.preventDefault();
+      });
+
 });
 
 /*
  * Function that is called when the document is ready.
  */
-function initializePage() {
-	console.log("Javascript connected!");
-};
+ function initializePage() {
+   console.log("Javascript connected!");
+   $('.glyphicon-share-alt').click(share);
+   $('.glyphicon-trash').click(trash);
+   $('.glyphicon-pencil').click(pencil);
+ };
+
+ function share(e) {
+  console.log("share");
+}
+
+function trash(e) {
+  console.log("trash");
+  ShowDialog(false);
+  e.preventDefault();
+}
+
+function deleteEntry(e) {
+  //this will eventually actually delete the entry
+  console.log("deleting");
+  $("#swipe1").fadeOut(500, function(){
+    console.log("hiding");
+    $("#swipe1").hide(500);
+  });
+}
+
+function pencil(e) {
+  console.log("edit");
+}
+
+   function ShowDialog(modal)
+   {
+      $("#overlay").show();
+      $("#dialog").fadeIn(300);
+
+      if (modal)
+      {
+         $("#overlay").unbind("click");
+      }
+      else
+      {
+         $("#overlay").click(function (e)
+         {
+            HideDialog();
+         });
+      }
+   }
+
+   function HideDialog()
+   {
+      $("#overlay").hide();
+      $("#dialog").fadeOut(300);
+   } 
+        
