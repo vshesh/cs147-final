f0f505f0bbca304eb444a0924ebd2649507f48a1
ashngu <ashngu@stanford.edu>
2014-02-13 23:12:02 -0800

    gestures on wishlist working

--- public/js/wishlist2.js
+++ public/js/wishlist2.js
@@ -2,82 +2,50 @@ var width = window.innerWidth;
 
 $(document).ready(function() {
 	initializePage();
-
   $(document).on('touchmove', function(e) {
     e.preventDefault();
   });
 });
 
-/*
- * Function that is called when the document is ready.
- */
-function initializePage() {
-	console.log("Javascript connected!");
-	geoFindMe();
-  swipeTiles();
-};
-
-function swipeTiles() {
-var wishlist_entry = document.getElementById("swipe1");
-var distance = 0;
-var direction;
-var delta = 0;
-$("#swipe1").css({
-  left: "0px",
-});
-
-var swiped = false;
-var readout = document.getElementById("swipe1");
- var hammertime = new Hammer(wishlist_entry);
-    hammertime.on("swipeleft", function(ev) {
-      if(!swiped) {
-        console.log("swipeleft");
-          $("#swipe1").animate({
-            left: -width + "px",
-          }, 500, 'easeOutCirc');
-          swiped = true;
-        }
-        });
-    hammertime.on("swiperight", function(ev) {
-      if(swiped) {
-        console.log("swiperight");
-        $("#swipe1").animate({
-          left: "0px",
-        }, 500, 'easeOutCirc');
-        swiped = false;
-        }
+ function initializePage() {
+   console.log("Javascript connected!");
+   geoFindMe();
+   swipeTiles();
+   $("#remove").click(function (e) {
+    $("#swipe1").fadeOut(500, function(){
+      console.log("hiding");
+      $("#swipe1").hide(500);
     });
- // var hammertime = new Hammer(container, { drag_max_touches: 0 , drag_block_vertical: true});
- //    hammertime.on("drag", function(ev) {
+    e.preventDefault();
+  });
+ };
 
- //        direction = ev.gesture.direction;
- //        distance = ev.gesture.distance;
- //        delta = distance - delta;
+ function swipeTiles() {
+  var wishlist_entry = document.getElementById("swipe1");
+  $("#swipe1").css({
+    left: "0px",
+  });
 
-
-
- //        readout.innerHTML = "Direction: " + direction + "</br>Distance: " + distance + "</br>Delta: " + delta;
- //        console.log("distance: " + ev.gesture.distance);
- //        console.log(direction);
- //        if (delta < 0) {
- //          distance = -ev.gesture.distance/15;
- //        }
- //        if (direction != "left" && direction != "right") {
- //          distance = 0;
- //        }
- //        if (distance > window.innerWidth) distance = 0;
- //        $("#swipe1").animate({
- //          left: "+="+distance+"px",
- //        }, 0);
- //    });
-
- //  var hammertime2 = new Hammer(container, { drag_max_touches: 0 });
- //    hammertime2.on("dragend", function(ev) {
- //        console.log("dragend");
- //        $("#swipe1").animate({
- //          left: "0px",
- //        }, 500, 'easeOutCirc');
- //    })
+  var swiped = false;
+  var hammertime = new Hammer(wishlist_entry);
+  hammertime.on("swipeleft", function(ev) {
+    if(!swiped) {
+      console.log("swipeleft");
+      $("#swipe1").animate({
+        left: -width + "px",
+      }, 500, 'easeOutCirc');
+      swiped = true;
+    }
+  });
+  hammertime.on("swiperight", function(ev) {
+    if(swiped) {
+      console.log("swiperight");
+      $("#swipe1").animate({
+        left: "0px",
+      }, 500, 'easeOutCirc');
+      swiped = false;
+    }
+  });
 }
 
 function geoFindMe() {
