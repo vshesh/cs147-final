417bc01e310cb217e135334ecdffbb7f7c3dd916
Avi Bagla <avibagla@gmail.com>
2014-02-20 22:15:44 -0800

    setting wishlist to work with database

--- routes/wishlist.js
+++ routes/wishlist.js
@@ -1,8 +1,21 @@
 var data = require('../data/data.json');
+var models = require('../models');
+
 
 exports.view = function(req, res) {
   req.session.lastPage = '/login';
-  res.render("wishlist", data[0]);
+  //res.render("wishlist", data[0]);
+	models.User
+		.find({'google_id':req.user.google_id})
+		.sort()
+		.exec(userCallback);
+
+	function userCallback(err, user){
+		if(err){console.log(err), res.send(500)};
+		res.render("wishlist", user[0]);
+	}
+
+
   console.log("this is the req")
   console.log(req.user.google_id);
   console.log("yes");
