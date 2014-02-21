0fba4e8c59cd983ef8617f0a1787b7582c343439
ashngu <ashngu@stanford.edu>
2014-02-20 20:45:07 -0800

    testing entry editor

---
+++ public/js/pasteats-editcreate2.js
@@ -0,0 +1,60 @@
+$(document).ready(function() {
+	initializePage();
+});
+
+function initializePage() {
+  console.log("Javascript connected!");
+//   // $('.glyphicon-share-alt').click(share);
+//   $('.glyphicon-trash').click(trash);
+//   $("#btnNo").click(function (e) {
+//     HideDialog();
+//     e.preventDefault();
+//   });
+
+//   $("#btnYes").click(function (e) {
+//     HideDialog();
+//     deleteEntry(e);
+//     e.preventDefault();
+//   });
+};
+
+// //  function share(e) {
+// //   console.log("share");
+// // }
+
+// function trash(e) {
+//   console.log("trash");
+//   ShowDialog(false);
+//   e.preventDefault();
+// }
+
+// function deleteEntry(e) {
+//   //this will eventually actually delete the entry
+//   console.log("deleting");
+//   $("#swipe1").fadeOut(500, function(){
+//     console.log("hiding");
+//     $("#swipe1").hide(500);
+//   });
+// }
+
+// function pencil(e) {
+//   console.log("edit");
+// }
+
+// function ShowDialog(modal){
+//   $("#overlay").show();
+//   $("#dialog").fadeIn(300);
+
+//   if (modal) {
+//    $("#overlay").unbind("click");
+//  } else {
+//    $("#overlay").click(function (e){
+//     HideDialog();
+//   });
+//  }
+// }
+
+// function HideDialog(){
+//   $("#overlay").hide();
+//   $("#dialog").fadeOut(300);
+// } 
