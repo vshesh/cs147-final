befd021c56f755b704038fd50da04fdeef050150
Avi Bagla <avibagla@gmail.com>
2014-02-25 09:40:45 -0800

    gave past eats a months name for cleaner date

--- routes/pasteats.js
+++ routes/pasteats.js
@@ -1,6 +1,7 @@
 var models = require('../models.js');
 var data = require('../data/data.json');
 
+//Returns data value
 var findByAttr = function(array, attr, value) {
     for(var i = 0; i < array.length; i++) {
         if(array[i].hasOwnProperty(attr) && array[i][attr] === value) {
@@ -10,6 +11,7 @@ var findByAttr = function(array, attr, value) {
     return undefined;
 }
 
+//Returns int
 var findIndexByAttr = function(array, attr, value) {
     for(var i = 0; i < array.length; i++) {
         if(array[i].hasOwnProperty(attr) && array[i][attr] === value) {
@@ -18,6 +20,19 @@ var findIndexByAttr = function(array, attr, value) {
     }
     return undefined;
 }
+
+//Allows for easy access to month names in the Date prototype.
+Date.prototype.monthNames = [
+    "January", "February", "March",
+    "April", "May", "June",
+    "July", "August", "September",
+    "October", "November", "December"
+];
+
+Date.prototype.getMonthName = function() {
+	    return this.monthNames[this.getMonth()];
+};
+
 
 exports.view = function(req, res) {
 	var user = findByAttr(data, 'google_id', req.user.google_id);
@@ -31,12 +46,15 @@ exports.viewById = function(req, res) {
   res.render('pasteats-entry');
 }
 
+
+
 exports.add = function(req, res) {
+	
 
 	var time = new Date();
 	var newEntry = {
 		'created_timestamp' : Date.now(),
-		'formatted_date': time.getMonth() + "/" + time.getDate() + "/" + (time.getYear() + 1900),
+		'formatted_date': time.getMonthName() + " " + time.getDate() + ", " + time.getFullYear(),
 		'title' : req.body.title,
 		'summary': req.body.summary,
 		'image' : req.body.image,
