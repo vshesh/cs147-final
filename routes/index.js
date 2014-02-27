349279540182d029d65c789e1c2c84ec152a3d1f
Avi Bagla <avibagla@gmail.com>
2014-02-27 02:40:18 -0800

    fixed heroku error with user

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
 exports.view = function(req, res) {
-  if(req.user) res.render('wishlist');
-  else res.render('login');
+  if(req.user) {
+  var user = data[findIndexByAttr(data, 'google_id', req.user.google_id)];
+  res.render('wishlist', user);
+	} else res.render('login');
+
 }
 
