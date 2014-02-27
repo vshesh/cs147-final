346ad81d9126c5c0f69a2051d6b81ce8832e88d5
Avi Bagla <avibagla@gmail.com>
2014-02-27 10:52:35 -0800

    using settings to keep our secrets out of the hands of evil people

--- routes/project.js
+++
@@ -1,8 +0,0 @@
-exports.viewProject = function(req, res) {
-  var name = req.params.name;
-  res.render('project', {
-    'jumbo-name': name, 
-    'jumbo-description': 'one-sentence description goes here',
-    'jumbo-id': 'project-jumbo'
-  });
-};
