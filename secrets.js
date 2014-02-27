46ca9b6d832469f3dc3e684390c6f233f93d3dfe
Avi Bagla <avibagla@gmail.com>
2014-02-27 11:11:42 -0800

    destroying old secrets

--- secrets.js
+++
@@ -1,4 +0,0 @@
-exports.googleID = '101989331854-aqje8r3qpvudqru12luln8q88kdm7btd.apps.googleusercontent.com';
-exports.googleSecret = '9DCgH6NzVtSZlMPwtodv2pLv';
-exports.umami = "http://umami.herokuapp.com/auth/google/callback";
-exports.local = "http://localhost:3000/auth/google/callback";
