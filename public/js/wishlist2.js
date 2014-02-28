f5ffbd2fedba100b2961892e6e2871788ac8c3ba
Avi Bagla <avibagla@gmail.com>
2014-02-28 12:09:52 -0800

    continuing cleanup, deleting shitty routes

--- public/js/wishlist2.js
+++ public/js/wishlist2.js
@@ -7,10 +7,13 @@ $(document).ready(function() {
 
 
 
-
  function initializePage() {
    // geoFindMe();
+
+   //Init Swiping
    swipeTiles();
+
+   //Handle removing a wishlist item.
    $('.glyphicon-remove').each(function(i,e) {
     $(e).click(function(ev) {
       ev.preventDefault();
@@ -28,32 +31,37 @@ $(document).ready(function() {
  });
  }
 
- function swipeTiles() {
 
+ function swipeTiles() {
+  //Swipe on each entry
   $('.wishlist-entry').each(function(i,e) {
     $(this).css({
       left: -width +"px"
     });
+    //Swipe open
     Hammer(e).on("swiperight", function(ev) {
-    if(!swiped) {
-      $(this).animate({
-        left: "0px"
-      }, 500, 'easeOutCirc');
-      swiped = true;
-    }
+      if(!swiped) {
+        $(this).animate({
+          left: "0px"
+        }, 500, 'easeOutCirc');
+        swiped = true;
+      }
+    });
   });
-  });
+  //Swipe close
   $('.wishlist-entry').each(function(i,e) {
     Hammer(e).on("swipeleft", function(ev) {
-    if(swiped) {
-      $(this).animate({
-        left: -width + "px",
-      }, 500, 'easeOutCirc');
-      swiped = false;
-    }
-  });
+      if(swiped) {
+        $(this).animate({
+          left: -width + "px",
+        }, 500, 'easeOutCirc');
+        swiped = false;
+      }
+    });
   });
 }
+
+//Find your location (to be implemented?)
 
 function geoFindMe() {
   var output = document.getElementById("out");
