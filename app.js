e53c36cc2848bab669ddc22752ab5f3bb394c5a9
Vishesh Gupta <vishesh@stanford.edu>
2014-02-27 23:45:06 -0800

    undoing letting searchalt through

--- app.js
+++ app.js
@@ -183,8 +183,6 @@ function ensureAuthenticated(req, res, next) {
       req.path.startsWith('/css/bootstrap') || 
       req.path.startsWith('/css/introHCI') ||
       req.path.startsWith('/css/login') ||
-      // REMOVE THIS WHEN JULIE GRADES THIS WEEK's ASSIGN 
-      req.path.startsWith('/searchalt') ||
       req.path.startsWith('/auth') || 
       req.isAuthenticated()) { return next(); }
   
